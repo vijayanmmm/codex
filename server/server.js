7bac64868d28bcdd7d4ea75c561fc6f23a98bc2c
Venkateshan Vijayan <vijayan_mmm@yahoo.com>
2023-03-09 17:32:00 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,8 +28,8 @@ app.post('/',async(req,res)=>{
     try {
         const prompt = req.body.prompt;
         //limit only 100 letters if its more
-        if (prompt.length > 100){
-            prompt = prompt.substring(0,99)
+        if (prompt.length > 1000){
+            prompt = prompt.substring(0,1000)
         }
         console.log("Input Prompt:",prompt)
 
@@ -37,7 +37,7 @@ app.post('/',async(req,res)=>{
             model: "text-davinci-003",
             prompt: `${prompt}`,
             temperature: 0,
-            max_tokens: 2000,
+            max_tokens: 5000,
             top_p: 1,
             frequency_penalty: 0.5,
             presence_penalty: 0,
